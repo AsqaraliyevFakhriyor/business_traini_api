9d0d8b5ceebc83e9fb0cd5fbd4dabab768c2a235
AsqaraliyevFakhriyor <asqaraliyev01@gmail.com>
2022-05-27 15:53:14 +0500

    Added new urls
    - Added login urls for login view
    - Added user url for user view

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, UserView
 
 app_name = "user"
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name="user_login"),
     path("login", LoginView.as_view(), name="login"),
+    path("user", UserView.as_view(), name="user")
 
 ]
