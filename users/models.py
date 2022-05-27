a625650a5bbd668dc3107eeb1a7f120e1da135c0
AsqaraliyevFakhriyor <asqaraliyev01@gmail.com>
2022-05-27 15:52:19 +0500

    Changed password length
    - Increased password length to work with hashed passwords

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     name = models.CharField(max_length=60)
     email = models.EmailField(max_length=60, unique=True)
-    password = models.CharField(max_length=80)
+    password = models.CharField(max_length=150)
     username = None
 
     USERNAME_FIELD = 'email'
