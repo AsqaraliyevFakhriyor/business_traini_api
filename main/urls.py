9315f4f42fb7bb86e6f5645b5c0a7b789652dea2
AsqaraliyevFakhriyor <asqaraliyev01@gmail.com>
2022-05-30 04:17:18 +0500

    changed urls to make more readable

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import application_list
+from .views import application_list, application_create, application_delete
 
 urlpatterns = [
     path("applications", application_list, name="application_list"),
+    path("create/application", application_create, name="application_create"),
+    path("delete/application", application_delete, name="application_delete")
 ]
