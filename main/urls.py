24e168fca513da5c0fe079f046d0944a315dad5b
AsqaraliyevFakhriyor <asqaraliyev01@gmail.com>
2022-05-30 20:36:25 +0500

    added contacted url

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import application_list, application_create, application_delete
+from .views import application_list, application_create, application_delete, application_contacted
 
 urlpatterns = [
     path("applications", application_list, name="application_list"),
     path("create/application", application_create, name="application_create"),
-    path("delete/application", application_delete, name="application_delete")
+    path("delete/application", application_delete, name="application_delete"),
+    path("contacted/application", application_contacted, name="application_contacted")
 ]
