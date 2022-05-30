475690cfff6d8b2e74feb8b990b855eb72c62ca8
AsqaraliyevFakhriyor <asqaraliyev01@gmail.com>
2022-05-30 16:54:49 +0500

    bug-fix

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 DEBUG = os.environ["DEBUG"]
 
-ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS']
+ALLOWED_HOSTS = ["alisherisayev.herokuapp.com"]
 
 
 # JWT CREDENTIALS
